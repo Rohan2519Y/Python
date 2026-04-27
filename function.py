cec7a9a8862515ff0f7e2d92a2149597f3f4884f
Rohan2519Y <rohankumar17645@gmail.com>
2026-04-27 19:01:33 +0530

    Update

--- function.py
+++ function.py
@@ -22,9 +22,50 @@
 # import myfile
 # from myfile import sqrt, factorial
 # 
+# 
+# 
+# call by value
+# in this tech. actual arg copied their values in formal arg and whatever changes will make in formal args will not affect the values of actual args
+# 
+# 
+# 
+# call by reference 
+# in this tech. actual arg copied their address in formal arg and whatever changes will make in formal args will not affect the reference of actual args 
+# 
+# 
 
-def SimpleInterest(p,r,t):
-    s=p*r*t/100
-    return s
+# def SimpleInterest(p,r,t):
+#     s=p*r*t/100
+#     return s
 
-print(SimpleInterest(100,200,3))
+# print(SimpleInterest(100,200,3))
+
+
+# def geteven(L):
+#     E=[]
+#     for i in L:
+#         if i%2==0:
+#             E.append(i)
+#     return E
+
+# L=[1,2,3,4]
+# T=geteven(L)
+# print(T)
+
+
+# def getHexa(L):
+#     E=[]
+#     for V in L:
+#         E.append(hex(V)[2:])
+#     return ''.join(E)
+# L=[123,34,54]
+# T=getHexa(L)
+# print(T)
+
+
+# def removeChar(S,C):
+#     return S.replace(C,'')
+
+# T=removeChar('eoeienhe','e')
+# print(T)
+
