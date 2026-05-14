9134b8ee65df6459ed6b781355494a5d90074cc7
Rohan2519Y <rohankumar17645@gmail.com>
2026-05-14 19:54:10 +0530

    SQL

--- sql.py
+++ sql.py
@@ -61,26 +61,135 @@ import pymysql as sql
 
 
 
-try:
-    DB = sql.connect(
-        host='localhost',
-        port=3306,
-        user='root',
-        password='1234',
-        database='pythonpractice'
-    )
+# try:
+#     DB = sql.connect(
+#         host='localhost',
+#         port=3306,
+#         user='root',
+#         password='1234',
+#         database='pythonpractice'
+#     )
 
-    SMT = DB.cursor()
-    EID = input("Enter Employee ID : ")
-    EName = input("Enter Employee Name : ")
-    ECity = input("Enter Employee City : ")
+#     SMT = DB.cursor()
+#     EID = input("Enter Employee ID : ")
+#     EName = input("Enter Employee Name : ")
+#     ECity = input("Enter Employee City : ")
 
-    Q = f"Insert into employees values({EID}, '{EName}', '{ECity}')"
-    print(Q)
-    SMT.execute(Q)
-    print('Database Created')
-    DB.commit()
-    DB.close()
+#     Q = f"Insert into employees values({EID}, '{EName}', '{ECity}')"
+#     print(Q)
+#     SMT.execute(Q)
+#     print('Database Created')
+#     DB.commit()
+#     DB.close()
 
-except Exception as e:
-    print("Error : ", e)
+# except Exception as e:
+#     print("Error : ", e)
+
+
+# Database my_industry
+# table products
+#   ProductId, ProductName, QuantityPerUnit, UnitPrice, UnitInStock, UnitsOnOrder, ReorderLevel, Discountinued, MFG Date
+# 
+# 
+# 
+# 
+# 
+
+
+# Show Data #####################################################################################################
+# try:
+#     DB = sql.connect(
+#         host='localhost',
+#         port=3306,
+#         user='root',
+#         password='1234',
+#         database='PythonPractice'
+#     )
+
+#     SMT = DB.cursor()
+#     Q = 'Select * from Employees'
+#     SMT.execute(Q)
+#     Record = SMT.fetchall()
+#     print(Record)
+#     DB.close()
+
+# except Exception as e:
+#     print("Error : ", e)
+
+
+# Dict #####################################################################################################
+# try:
+#     DB = sql.connect(
+#         host = 'localhost',
+#         port = 3306,
+#         user = 'root',
+#         password = '1234',
+#         database = 'PythonPractice',
+#         cursorclass = sql.cursors.DictCursor
+#     )
+
+#     SMT = DB.cursor()
+#     Q = 'Select * from Employees'
+#     SMT.execute(Q)
+#     Record = SMT.fetchall()
+#     print(Record[0]['employeeid'])
+#     DB.close()
+
+# except Exception as e:
+#     print("Error : ", e)
+
+
+
+# Search #####################################################################################################
+# try:
+#     DB = sql.connect(
+#         host = 'localhost',
+#         port = 3306,
+#         user = 'root',
+#         password = '1234',
+#         database = 'PythonPractice',
+#         cursorclass = sql.cursors.DictCursor
+#     )
+
+#     SMT = DB.cursor()
+#     inpt=input("Enter ID : ")
+#     Q = f'Select * from Employees where employeeid={inpt}'
+#     SMT.execute(Q)
+#     Record = SMT.fetchall()
+#     if Record : 
+#         print(Record)
+#     else : 
+#         print("Record Not Found")
+#     DB.close()
+
+# except Exception as e:
+#     print("Error : ", e)
+
+
+
+
+# Range #####################################################################################################
+# try:
+#     DB = sql.connect(
+#         host = 'localhost',
+#         port = 3306,
+#         user = 'root',
+#         password = '1234',
+#         database = 'PythonPractice',
+#         cursorclass = sql.cursors.DictCursor
+#     )
+
+#     SMT = DB.cursor()
+#     min=input("Enter Min : ")
+#     max=input("Enter Max : ")
+#     Q = f'Select * from Employees where city between {min} and {max}'
+#     SMT.execute(Q)
+#     Record = SMT.fetchall()
+#     if Record : 
+#         print(Record)
+#     else : 
+#         print("Record Not Found")
+#     DB.close()
+
+# except Exception as e:
+#     print("Error : ", e)
