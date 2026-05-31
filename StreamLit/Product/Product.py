ec817ea613ea8c0d186cb0121148f839f4dccf88
Rohan2519Y <rohankumar17645@gmail.com>
2026-05-31 15:56:59 +0530

    Update

--- StreamLit/Product/Product.py
+++ StreamLit/Product/Product.py
@@ -24,164 +24,162 @@ st.set_page_config(
 
 
 
-# st.sidebar.title("Product")
+st.sidebar.title("Product")
 
 # menu = st.sidebar.radio(
 #     "Select Option",
 #     ["Home", "Display", "Insert", "Update", "Delete", "Search"]
 # )
 
-# if menu == "Home":
-#     st.image("./product.jfif",width = 500)
-#     st.title("Product Management System")
-#     st.write("Display Data")
-#     st.write("Update Data")
-#     st.write("Delete Data")
-#     st.write("Search Data")
+with st.sidebar:
+    menu = option_menu(
+        menu_title=None,
+        options=["Home", "Display", "Insert", "Update", "Delete", "Search"],
+        icons=["house", "table", "plus-circle", "pencil-square", "trash", "search"],
+        orientation="vertical"
+    )
 
-# elif menu == "Display":
-#     st.title("Display")    
-#     SMT.execute('Select * from products')
-#     Record = SMT.fetchall()
-#     DB.close()
-#     if Record :
-#         st.dataframe(Record)
-#     else :
-#         st.error("Record Not Found")
+if menu == "Home":
+    st.image("../product.jfif",width = 500)
+    st.title("Product Management System")
+    st.write("Display Data")
+    st.write("Update Data")
+    st.write("Delete Data")
+    st.write("Search Data")
 
-# elif menu == "Insert" :
-#     st.title("Insert Data")
-#     ProductId = st.text_input("Enter Product Id")
-#     ProductName = st.text_input("Enter Product Name")
-#     QuantityPerUnit = st.text_input("Enter Quantity Per Unit")
-#     UnitPrice = st.text_input("Enter Unit Price")
-#     UnitInStock = st.text_input("Enter Unit In Stock")
-#     UnitsOnOrder = st.text_input("Enter Units on Order")
-#     ReorderLevel = st.text_input("Enter Reorder Level")
-#     Discountinued = st.text_input("Enter Discountinued")
-#     MFG = st.date_input("Enter MFG")
+elif menu == "Display":
+    st.title("Display")    
+    SMT.execute('Select * from products')
+    Record = SMT.fetchall()
+    DB.close()
+    if Record :
+        st.dataframe(Record)
+    else :
+        st.error("Record Not Found")
 
-#     if st.button("Submit") : 
-#         try :
-#             SMT.execute(f'insert into products values ("{ProductId}", "{ProductName}", "{QuantityPerUnit}", "{UnitPrice}", "{UnitInStock}", "{UnitsOnOrder}", "{ReorderLevel}", "{Discountinued}", "{MFG}")')
-#             DB.commit()
-#             st.success("Record Submitted Successfully")
-#         except Exception as E :
-#             st.error(e)
-#         finally :
-#             DB.close()
+elif menu == "Insert" :
+    st.title("Insert Data")
+    ProductId = st.text_input("Enter Product Id")
+    ProductName = st.text_input("Enter Product Name")
+    QuantityPerUnit = st.text_input("Enter Quantity Per Unit")
+    UnitPrice = st.text_input("Enter Unit Price")
+    UnitInStock = st.text_input("Enter Unit In Stock")
+    UnitsOnOrder = st.text_input("Enter Units on Order")
+    ReorderLevel = st.text_input("Enter Reorder Level")
+    Discountinued = st.text_input("Enter Discountinued")
+    MFG = st.date_input("Enter MFG")
 
-# elif menu == "Update" :
-#     st.title("Update Data")
-#     ProductId = st.text_input("Enter Product Id You Want to Update")
-#     Fetch = st.button("Fetch Data")
+    if st.button("Submit") : 
+        try :
+            SMT.execute(f'insert into products values ("{ProductId}", "{ProductName}", "{QuantityPerUnit}", "{UnitPrice}", "{UnitInStock}", "{UnitsOnOrder}", "{ReorderLevel}", "{Discountinued}", "{MFG}")')
+            DB.commit()
+            st.success("Record Submitted Successfully")
+        except Exception as E :
+            st.error(e)
+        finally :
+            DB.close()
 
-#     if Fetch :
-#         SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
-#         Record = SMT.fetchone()
+elif menu == "Update" :
+    st.title("Update Data")
+    ProductId = st.text_input("Enter Product Id You Want to Update")
+    Fetch = st.button("Fetch Data")
 
-#         if Record :
-#             st.session_state.Show = True
-#             st.session_state.Record = Record
-#             st.session_state.ProductId = ProductId
+    if Fetch :
+        SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
+        Record = SMT.fetchone()
 
-#         else :
-#             st.success("Record Not Found")
+        if Record :
+            st.session_state.Show = True
+            st.session_state.Record = Record
+            st.session_state.ProductId = ProductId
 
-#     if st.session_state.get("Show", False) :
-#         Record = st.session_state.Record
-#         ProductName = st.text_input("Enter Product Name", value = Record['ProductName'])
-#         QuantityPerUnit = st.text_input("Enter Quantity Per Unit", value = Record['QuantityPerUnit'])
-#         UnitPrice = st.text_input("Enter Unit Price", value = Record['UnitPrice'])
-#         UnitInStock = st.text_input("Enter Unit In Stock", value = Record['UnitInStock'])
-#         UnitsOnOrder = st.text_input("Enter Units on Order", value = Record['UnitsOnOrder'])
-#         ReorderLevel = st.text_input("Enter Reorder Level", value = Record['ReorderLevel'])
-#         Discountinued = st.text_input("Enter Discountinued", value = Record['Discountinued'])
-#         MFG = st.date_input("Enter MFG", value = Record['MFG'])
+        else :
+            st.success("Record Not Found")
 
-#         if  st.button("Submit") :
-#             try :
-#                 Q = f'update products set ProductName = "{ProductName}", QuantityPerUnit = "{QuantityPerUnit}", UnitPrice = "{UnitPrice}", UnitInStock = "{UnitInStock}", UnitsOnOrder = "{UnitsOnOrder}", ReorderLevel = "{ReorderLevel}", Discountinued = "{Discountinued}", MFG = "{MFG}" where ProductId = "{st.session_state.ProductId}"'
-#                 SMT.execute(Q)
-#                 DB.commit()
-#                 st.success("Record Updated Successfully")
-#                 st.session_state.Show = False
+    if st.session_state.get("Show", False) :
+        Record = st.session_state.Record
+        ProductName = st.text_input("Enter Product Name", value = Record['ProductName'])
+        QuantityPerUnit = st.text_input("Enter Quantity Per Unit", value = Record['QuantityPerUnit'])
+        UnitPrice = st.text_input("Enter Unit Price", value = Record['UnitPrice'])
+        UnitInStock = st.text_input("Enter Unit In Stock", value = Record['UnitInStock'])
+        UnitsOnOrder = st.text_input("Enter Units on Order", value = Record['UnitsOnOrder'])
+        ReorderLevel = st.text_input("Enter Reorder Level", value = Record['ReorderLevel'])
+        Discountinued = st.text_input("Enter Discountinued", value = Record['Discountinued'])
+        MFG = st.date_input("Enter MFG", value = Record['MFG'])
 
-#             except Exception as E :
-#                 st.error(E)
+        if  st.button("Submit") :
+            try :
+                Q = f'update products set ProductName = "{ProductName}", QuantityPerUnit = "{QuantityPerUnit}", UnitPrice = "{UnitPrice}", UnitInStock = "{UnitInStock}", UnitsOnOrder = "{UnitsOnOrder}", ReorderLevel = "{ReorderLevel}", Discountinued = "{Discountinued}", MFG = "{MFG}" where ProductId = "{st.session_state.ProductId}"'
+                SMT.execute(Q)
+                DB.commit()
+                st.success("Record Updated Successfully")
+                st.session_state.Show = False
 
-# elif menu == "Delete" :
-#     st.title("Delete Data")
-#     ProductId = st.text_input("Enter Product Id You Want to Delete")
+            except Exception as E :
+                st.error(E)
 
-#     if st.button("Fetch Data") :
-#         SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
-#         Record = SMT.fetchone()
+elif menu == "Delete" :
+    st.title("Delete Data")
+    ProductId = st.text_input("Enter Product Id You Want to Delete")
 
-#         if Record :
-#             st.session_state.Show = True
-#             st.session_state.ProductId = ProductId
-#         else :
-#             st.info("Record Not Found")
-            
-#     if st.session_state.get("Show", False) :        
-#         st.write("Do You Want to Delete")
-#         if st.button("Yes"):
-#             try :
-#                 SMT.execute(f'Delete from products where ProductId = "{st.session_state.ProductId}"')
-#                 DB.commit()
-#                 st.success("Record Deleted Successfully")
-#                 st.session_state.Show = False
-#             except Exception as E :
-#                 st.error(e)
-#             finally :
-#                 DB.close()
-#         elif st.button("No") :
-#             st.info("Delete Cancel")
+    @st.dialog("Do you want to delete")
+    def Dialog(Rec):
+        st.dataframe(Rec)
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button("Yes"):
+                try :
+                    SMT.execute(f'Delete from products where ProductId = "{st.session_state.ProductId}"')
+                    DB.commit()
+                    st.success("Record Deleted Successfully")
+                except Exception as E :
+                    st.error(E)
+                finally :
+                    DB.close()
+                    st.rerun()
+        with col2 :
+            if st.button("No") :
+                st.info("Delete Cancel")
 
-# elif menu == "Search" :
-#     st.title("Search Data")
-#     ProductId = st.text_input("Enter Product Id You Want to Search")
+    if st.button("Fetch Data") :
+        SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
+        Record = SMT.fetchone()
 
-#     if st.button("Fetch Data") :
-#         SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
-#         Record = SMT.fetchall()
+        if Record :
+            st.session_state.ProductId = ProductId
+            Dialog(Record)
+        else :
+            st.info("Record Not Found")
 
-#         if Record :
-#             st.dataframe(Record)
-#             st.success("Data Fetched Successfully")
-#         else :
-#             st.info("Record Not Found")
+elif menu == "Search" :
+    st.title("Search Data")
+    ProductId = st.text_input("Enter Product Id You Want to Search")
 
-# else :
-#     st.image("./product.jfif", width = 500)
-#     st.title("Product Management System")
-#     st.write("Display Data")
-#     st.write("Update Data")
-#     st.write("Delete Data")
-#     st.write("Search Data")
+    if st.button("Fetch Data") :
+        SMT.execute(f'Select * from products where ProductId = "{ProductId}"')
+        Record = SMT.fetchall()
+
+        if Record :
+            st.dataframe(Record)
+            st.success("Data Fetched Successfully")
+        else :
+            st.info("Record Not Found")
+
+else :
+    st.image("./product.jfif", width = 500)
+    st.title("Product Management System")
+    st.write("Display Data")
+    st.write("Update Data")
+    st.write("Delete Data")
+    st.write("Search Data")
 
 
 
 #####################################################################################################
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title=None,
-        options=["Home", "Display", "Insert", "Update", "Delete", "Search"],
-        icons=["house", "people", "box", "telephone"],
-        orientation="vertical"
-    )
-
-if selected == 'Home' :
-    st.title("Product Management")
-if selected == 'Display' :
-    st.page_link('pages/Display.py')
-if selected == 'Insert' :
-    st.page_link('pages/Insert.py')
-if selected == 'Update' :
-    st.page_link('pages/Update.py')
-if selected == 'Delete' :
-    st.page_link('pages/Delete.py')
-if selected == 'Search' :
-    st.page_link('pages/Search.py')
+# st.title("Product Management")
+# st.page_link('pages/Display.py')
+# st.page_link('pages/Insert.py')
+# st.page_link('pages/Update.py')
+# st.page_link('pages/Delete.py')
+# st.page_link('pages/Search.py')
